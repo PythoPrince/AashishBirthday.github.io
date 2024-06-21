11a7154509e53c04cdc96b0c207a534796fecfb2
Prince Kumar <143723542+PythoPrince@users.noreply.github.com>
2024-06-22 01:18:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -18,6 +18,22 @@ function showSlides() {
     setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
 
+function currentSlide(n) {
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {n = 1}
+    if (n < 1) {n = slides.length}
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[n-1].style.display = "block";  
+    dots[n-1].className += " active";
+    slideIndex = n - 1;
+}
+
 function playSong() {
     let audio = document.getElementById("birthdaySong");
     audio.play();
